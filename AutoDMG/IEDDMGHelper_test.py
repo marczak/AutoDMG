5b7dc58dd8a365d2f47f6bb2d85f95e27cf23fac
Edward Marczak <germ@google.com>
2014-10-03 14:27:31 -0400

    Begin IEDDMGHelper tests.

--- AutoDMG/IEDDMGHelper_test.py
+++ AutoDMG/IEDDMGHelper_test.py
@@ -5,3 +5,10 @@
 #
 #  Created by Edward Marczak on 9/8/14.
 #  Copyright (c) 2014 University of Gothenburg. All rights reserved.
+
+import unittest
+import mock
+import IEDDMGHelper
+
+class DMGTests(unittest.TestCase):
+  
